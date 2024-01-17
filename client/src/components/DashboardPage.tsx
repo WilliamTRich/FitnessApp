5abf63c1797cced6da20770f819b1260b60ea49f
Will <williamrich@homerak.net>
2024-01-16 16:10:21 -0900

    Started layout for dashboard page

--- client/src/components/DashboardPage.tsx
+++ client/src/components/DashboardPage.tsx
@@ -1,9 +1,25 @@
 import Navbar from "./Navbar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 function DashboardPage() {
   return (
     <div className="flex h-full w-full">
       <Navbar />
+      <main className="flex py-10 justify-evenly">
+        <ScrollArea className="h-full w-2/5 rounded-md border-2 p-4">
+          Jokester began sneaking into the castle in the middle of the night and
+          leaving jokes all over the place: under the king's pillow, in his
+          soup, even in the royal toilet. The king was furious, but he couldn't
+          seem to stop Jokester. And then, one day, the people of the kingdom
+          discovered that the jokes left by Jokester were so funny that they
+          couldn't help but laugh. And once they started laughing, they couldn't
+          stop.
+        </ScrollArea>
+        <div className="flex flex-col w-2/5 gap-6">
+          <div className="flex-1 border-2 rounded-md"></div>
+          <div className="flex-1 border-2 rounded-md"></div>
+        </div>
+      </main>
     </div>
   );
 }
