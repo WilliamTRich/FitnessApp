086333e2269f59ae3473ffdd41c6264699c24373
Will <williamrich@homerak.net>
2024-01-22 14:57:32 -0900

    Added calendar buttons

---
+++ client/src/components/AddCalendarEvent.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+interface AddCalendarEventProps {
+  buttonText: string;
+  title: string;
+  description: string;
+  label: string;
+}
+
+function AddCalendarEvent({
+  buttonText,
+  title,
+  description,
+  label,
+}: AddCalendarEventProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="text-4xl h-24">
+          {buttonText}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="event" className="text-right">
+              {label}
+            </Label>
+            <Input id="event" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="description" className="text-right">
+              Description
+            </Label>
+            <Textarea id="description" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="time" className="text-right">
+              Time
+            </Label>
+            <Input id="time" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AddCalendarEvent;
--- client/src/components/CalendarPage.tsx
+++ client/src/components/CalendarPage.tsx
@@ -2,10 +2,11 @@ import Navbar from "./Navbar";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "./styles/calendar.scss";
+import AddCalendarEvent from "./AddCalendarEvent";
 
 const localizer = momentLocalizer(moment);
 
-//TODO - Fix styling, add buttons for events.
+//TODO - Add baseline for events, set up props.
 
 function CalendarPage() {
   return (
@@ -16,9 +17,29 @@ function CalendarPage() {
           localizer={localizer}
           startAccessor="start"
           endAccessor="end"
+          toolbar={false}
           className="h-[700px] w-[1000px]"
         />
-        <div className="flex flex-col h-full w-[500px] justify-center items-center border-2"></div>
+        <div className="flex flex-col h-[700px] w-[500px] justify-evenly items-center border-2 rounded-lg">
+          <AddCalendarEvent
+            buttonText="Add a Workout"
+            title="Workout"
+            description="Add a workout, go over specifics!"
+            label="Workout"
+          />
+          <AddCalendarEvent
+            buttonText="Add an Event"
+            title="Event"
+            description="Add a life event, go over specifics!"
+            label="Event"
+          />
+          <AddCalendarEvent
+            buttonText="Add a Meal"
+            title="Meal"
+            description="Add a meal plan, go over specifics!"
+            label="Meal"
+          />
+        </div>
       </main>
     </div>
   );
